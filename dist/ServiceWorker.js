711caa3e5c3df5b3ff5327c947264d3ef9e30327
Romain <romain.gazielly@gmail.com>
2018-08-08 18:56:32 +0400

    Service Worker

--- dist/ServiceWorker.js
+++ dist/ServiceWorker.js
@@ -1,8 +1,22 @@
+var CACHE_NAME = 'DubaiQuizv1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/*',
+  '/js/*'
+];
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/ServiceWorker.js').then(function(registration) {
       // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      if(reg.installing) {
+        console.log('Service worker installing');
+      } else if(reg.waiting) {
+        console.log('Service worker installed');
+      } else if(reg.active) {
+        console.log('Service worker active');
+      }
     }, function(err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
@@ -10,20 +24,17 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-var CACHE_NAME = 'DubaiQuizv1';
-var urlsToCache = [
-  '/',
-  '/css/*',
-  '/js/*'
-];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
+  console.log('WORKER: install event in progress.');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(urlsToCache)
+        .then(function() {
+          console.log('WORKER: install completed');
+        })
       })
   );
 });
