9c5ab9e7d23b28f3f150dae470d95bc611c673b4
Romain <romain.gazielly@gmail.com>
2018-08-09 11:23:10 +0400

    Service Worker

--- dist/ServiceWorker.js
+++ dist/ServiceWorker.js
@@ -1,4 +1,4 @@
-var cacheName = 'DubaiQuizv2';
+var CACHE_NAME = 'DubaiQuizv2';
 var filesToCache = [
   'https://fonts.googleapis.com/css?family=Roboto:100:300,400,500,700,900|Material+Icons',
   '/**/*.{js,html,css}',
@@ -23,7 +23,7 @@ function installWorkers(){
   self.addEventListener('install', function(event) {
     console.log('WORKER: install event in progress.');
     event.waitUntil(
-      caches.open(cacheName)
+      caches.open(CACHE_NAME)
         .then(function(cache) {
           console.log('Opened cache');
           return cache.addAll(filesToCache)
@@ -43,9 +43,60 @@ function installWorkers(){
           if (response) {
             return response;
           }
-          return fetch(event.request);
-        }
-      )
+
+          // IMPORTANT: Clone the request. A request is a stream and
+          // can only be consumed once. Since we are consuming this
+          // once by cache and once by the browser for fetch, we need
+          // to clone the response.
+          var fetchRequest = event.request.clone();
+
+          return fetch(fetchRequest).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+
+              return response;
+            }
+          );
+        })
     );
+  });
+
+
+  self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['DubaiQuizv2'];
+
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+
+
+  cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
+    return new Request(urlToPrefetch, { mode: 'no-cors' });
+  })).then(function() {
+    console.log('All resources have been fetched and cached.');
   });
 }
