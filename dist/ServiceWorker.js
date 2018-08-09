95ffbf448404216568f90995865f2f675fc18dba
Romain <romain.gazielly@gmail.com>
2018-08-09 11:25:54 +0400

    Service Worker

--- dist/ServiceWorker.js
+++ dist/ServiceWorker.js
@@ -93,10 +93,4 @@ function installWorkers(){
     );
   });
 
-
-  cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
-    return new Request(urlToPrefetch, { mode: 'no-cors' });
-  })).then(function() {
-    console.log('All resources have been fetched and cached.');
-  });
 }
