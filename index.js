ecb3eec158081742427a42e69af4598951819e49
Reda Bourial <reda.bourial@gmail.com>
2023-08-19 16:40:53 +0200

    added prod script

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+// Constants
+const dist = `${__dirname}/dist`;
+const port = 81 ;
+
+// Setup app
+const app = express()
+app.use(express.json());
+app.use(express.static(dist));
+
+app.all('/end-of-quiz', (req, res, next) => {
+  // const { product, machines } = req.body;
+  const answer =[
+    {
+        "user":"romain",
+        "score":50,
+    },
+    {
+        "user":"reda",
+        "score":10,
+    },
+  ];
+  res.send(JSON.stringify(answer));
+})
+
+app.listen(port);
